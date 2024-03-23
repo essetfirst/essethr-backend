35b78c5bb0d3e3915baebe9727b1e6b1e18d31e8
Abraham Gebrekidan <81012491+abrahamgkidan@users.noreply.github.com>
2024-03-23 12:45:54 +0300

    Update mongodb uri
    Updated the production MongoDB connection uri to the latest one.

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,10 @@
 module.exports = {
   db: {
-    prodMongodbURI:
-      "mongodb+srv://abraham:developerIAM2020@cluster0.dk8dc.gcp.mongodb.net/payrollex?retryWrites=true&w=majority",
+    // prodMongodbURI: "mongodb+srv://abraham:developerIAM2020@cluster0.dk8dc.gcp.mongodb.net/payrollex?retryWrites=true&w=majority",
+    prodMongodbURI: "mongodb+srv://abrahamgkidan:QAKCSaoZO6142ltu@cluster0.pay9hdw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", 
     devMongodbURI: "mongodb://localhost:27017",
     dbName: "payrollex",
+    
   },
   auth: {
     jwtSecret: "234234dfgsdtbskdfsuidfsdfnsdmfsdf,sfsdfskfsfd",
